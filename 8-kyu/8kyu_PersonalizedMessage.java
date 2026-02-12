8b9acdda9fab2fbad40f9950bdce7c57dd53b705
Henrique <cdhnr@icloud.com>
2026-02-12 08:31:50 -0300

    Add personalized greeting function
    Implemented a function to return a personalized greeting based on name and owner.

---
+++ 8-kyu/8kyu_PersonalizedMessage.java
@@ -0,0 +1,20 @@
+/**
+ * Personalized Message
+ *
+ * Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+ * Use conditionals to return the proper message:
+ * - If name equals owner → return "Hello boss"
+ * - Otherwise → return "Hello guest"
+ *
+ */
+
+class Kata {
+    static String greet(String name, String owner) {
+        // Add code here      
+      if (name.equals(owner))  {
+        return "Hello boss";
+      } else {
+        return "Hello guest";
+      }
+    }      
+}
