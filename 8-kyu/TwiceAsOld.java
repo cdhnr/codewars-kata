a6358965e00435715258f52bb5f9ded1da01f1a9
cdhnr <cdhnr@icloud.com>
2026-03-07 17:42:48 -0300

    Add TwiceAsOld class

---
+++ 8-kyu/TwiceAsOld.java
@@ -0,0 +1,18 @@
+/**
+ * Twice As Old
+ *
+ * Your function takes two arguments:
+ * current father's age (years)
+ * current age of his son (years)
+ * Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+ * The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+ *
+ */
+
+public class TwiceAsOld {
+
+    public static int twiceAsOld(int dadYears, int sonYears) {
+        return sonYears == 0 ? dadYears : Math.abs(dadYears - (sonYears * 2));
+    }
+
+}
